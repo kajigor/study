58368ca631c7b5f3b5e55a006828076f1f6a803f
Ekaterina Verbitskaia <kajigor@gmail.com>
2021-11-26 00:16:40 +0300

    Generating extension (no while)

--- generatingExtension/src/GenExt.hs
+++ generatingExtension/src/GenExt.hs
@@ -2,15 +2,27 @@
 
 module GenExt where
 
-import L
-import Control.Monad.Trans.State
+import L ( Input, L, Stmt(..), Expr(..), Op(..) )
+import Control.Monad.Trans.State ()
 import Control.Monad.Trans.Class ()
 import Text.Printf (printf)
 import Data.Text (Text)
+import Data.Maybe (fromMaybe)
 import Prettyprinter
+    ( (<+>),
+      hang,
+      indent,
+      layoutPretty,
+      line,
+      vsep,
+      parens,
+      Doc,
+      LayoutOptions(LayoutOptions, layoutPageWidth),
+      PageWidth(AvailablePerLine),
+      Pretty(pretty) )
 import Parser.Parser (parser)
-import Prettyprinter.Render.Text
-import Data.Text.IO
+import Prettyprinter.Render.Text ( renderStrict )
+import Data.Text.IO ( writeFile )
 
 type HS = Doc Text
 
@@ -21,6 +33,13 @@ preamble = vsep
   , ""
   , "import Control.Monad.Trans.State"
   , "import Control.Monad.Trans.Class"
+  , ""
+  , "intToBool :: Int -> Bool"
+  , "intToBool x = x /= 0"
+  , ""
+  , "boolToInt :: Bool -> Int"
+  , "boolToInt True = 1"
+  , "boolToInt False = 0"
   , ""
   ]
 
@@ -41,8 +60,25 @@ evalLGE program input =
 
 
 evalOpGE :: Op -> HS
-evalOpGE Plus = "(+)"
-evalOpGE Mult = "(*)"
+evalOpGE Plus  = "(+)"
+evalOpGE Minus = "(-)"
+evalOpGE Mult  = "(*)"
+evalOpGE Div   = "div"
+evalOpGE Pow   = "(^)"
+evalOpGE Eq    = transformCompareGE "=="
+evalOpGE Neq   = transformCompareGE "/="
+evalOpGE Lt    = transformCompareGE "<"
+evalOpGE Le    = transformCompareGE "<="
+evalOpGE Gt    = transformCompareGE ">"
+evalOpGE Ge    = transformCompareGE ">="
+evalOpGE And   = transformGE "&&"
+evalOpGE Or    = transformGE "||"
+
+transformCompareGE :: HS -> HS
+transformCompareGE op = "(\\x y -> boolToInt $ x " <> op <> " y)"
+
+transformGE :: HS -> HS
+transformGE op = "(\\x y -> boolToInt $ intToBool x " <> op <> " intToBool y)"
 
 indentOnce :: [Doc ann] -> Doc ann
 indentOnce = indent tabsize . vsep
@@ -101,15 +137,59 @@ evalStmtGE (Assign v e) =
            , hangOnce [ "case r of"
                       , "Just x -> do"
                       , indentOnce [ "put ((\"" <> pretty v <> "\", x) : state, input, output)"
-                                    , "return ()"
-                                    ]
+                                   , "return ()"
+                                   ]
                       , "Nothing -> lift Nothing"
                       ]
            ]
+evalStmtGE (If c thn els) =
+  let cComp = evalExprGE c in
+  hangOnce [ "do"
+           , "(state, _, _) <- get"
+           , "let cond = evalStateT (" <> cComp <> ") state"
+           , hangOnce [ "case cond of"
+                      , "Just condition -> "
+                      , indentOnce [ "if intToBool condition"
+                                   , "then "
+                                   , indentOnce
+                                      [hangOnce [ "do"
+                                      , compileStmts thn
+                                      ]]
+                                   , "else"
+                                   , indentOnce
+                                      [hangOnce [ "do"
+                                      , compileStmts els
+                                      ]]
+                                   ]
+                      , "Nothing -> lift Nothing"
+                      ]
+           ]
+-- does not terminate
+-- evalStmtGE w@(While c body) =
+--   let cComp = evalExprGE c in
+--   hangOnce [ "do"
+--            , "(state, _, _) <- get"
+--            , "let cond = evalStateT (" <> cComp <> ") state"
+--            , hangOnce [ "case cond of"
+--                       , "Just condition -> "
+--                       , hangOnce [ "when"
+--                                  , "(intToBool cond)"
+--                                  , hangOnce [ "(do"
+--                                             , "(do "
+--                                             , compileStmts body <> ")"
+--                                             , evalStmtGE w <> ")"
+--                                             ]
+--                                  ]
+--                       , "Nothing -> lift Nothing"
+--                       ]
+--            ]
+
+compileStmts stmts =
+  (indentThrice $ map (parens . evalStmtGE) stmts)
 
 evalLHelper :: L -> Input -> HS
 evalLHelper program inputs =
-  let compiledStatements = indentThrice $ map (parens . evalStmtGE) program in
+  let compiledStatements = compileStmts program  in
   indentOnce [ "let compiled = do " <> line <> compiledStatements
              , "let res = execStateT compiled ([]," <+> pretty inputs <> ", [])"
              , "print $" <+> (hangOnce [ "case res of"
@@ -118,13 +198,17 @@ evalLHelper program inputs =
                                        ])
              ]
 
+genExt :: Input -> L -> Maybe FilePath -> IO ()
+genExt inputs program path =
+    let fileName = fromMaybe "test/out/Output.hs" path in
+    Data.Text.IO.writeFile fileName (renderStrict . layoutPretty layoutOptions $ evalLGE program inputs)
+  where
+    layoutOptions = LayoutOptions { layoutPageWidth = AvailablePerLine 80 1 }
+
 run :: Input -> String -> Maybe FilePath -> IO ()
 run inputs string path =
     case parser string of
       Left err -> fail err
       Right program ->
-        let fileName = maybe "src/Output.hs" id path in
-        Data.Text.IO.writeFile fileName (renderStrict . layoutPretty layoutOptions $ evalLGE program inputs)
-  where
-    layoutOptions = LayoutOptions { layoutPageWidth = AvailablePerLine 80 1 }
+        genExt inputs program path
 
