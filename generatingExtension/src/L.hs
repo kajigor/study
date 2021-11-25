4f2ff80394885a41ed647dbb1f2e34da8e234163
Ekaterina Verbitskaia <kajigor@gmail.com>
2021-11-25 23:35:42 +0300

    If and While added

--- generatingExtension/src/L.hs
+++ generatingExtension/src/L.hs
@@ -3,7 +3,18 @@ module L where
 type Var = String
 
 data Op = Plus
+        | Minus
         | Mult
+        | Div
+        | Pow
+        | Eq
+        | Neq
+        | Lt
+        | Le
+        | Gt
+        | Ge
+        | And
+        | Or
         deriving Show
 
 data Expr = Lit Int
@@ -14,6 +25,8 @@ data Expr = Lit Int
 data Stmt = Assign Var Expr
           | Read Var
           | Write Expr
+          | If Expr L L
+          | While Expr L
           deriving Show
 
 type L = [Stmt]
